a4966f8d559ab69c31569b621089b5e82bc43064
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-19 09:11:28 -0700

    Moved to use fixed arguments

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -2,27 +2,171 @@ var assert = require('assert');
 var callOnce = require('../..');
 
 describe('callOnce', function () {
-  it('does not call multiple times', function (done) {
-    var results = [];
+  describe('does not call multiple times', function () {
+    var args = [];
 
-    function addResults() {
-      results.push(arguments);
+    function addArguments() {
+      args.push(Array.prototype.slice.call(arguments, 0));
     }
 
-    var callback1 = callOnce(addResults);
-    assert.ok(!results.length);
-    callback1('error', 'value1', 'value2');
-    assert.ok(!results.length);
-
-    setImmediate(function () {
-      assert.equal(results.length, 1);
-      callback1('error', 'value1', 'value2');
-      assert.equal(results.length, 1);
+    it('0 arguments', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1();
+      assert.ok(!args.length);
 
       setImmediate(function () {
-        assert.equal(results.length, 1);
-        done();
+        assert.equal(args.length, 1);
+        callback1();
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], []);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
       }, 10);
-    }, 10);
+    });
+
+    it('1 argument', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1(1);
+      assert.ok(!args.length);
+
+      setImmediate(function () {
+        assert.equal(args.length, 1);
+        callback1(101);
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], [1]);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
+      }, 10);
+    });
+
+    it('2 arguments', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1(1, 2);
+      assert.ok(!args.length);
+
+      setImmediate(function () {
+        assert.equal(args.length, 1);
+        callback1(101, 102);
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], [1, 2]);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
+      }, 10);
+    });
+
+    it('3 arguments', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1(1, 2, 3);
+      assert.ok(!args.length);
+
+      setImmediate(function () {
+        assert.equal(args.length, 1);
+        callback1(101, 102, 103);
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], [1, 2, 3]);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
+      }, 10);
+    });
+
+    it('4 arguments', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1(1, 2, 3, 4);
+      assert.ok(!args.length);
+
+      setImmediate(function () {
+        assert.equal(args.length, 1);
+        callback1(101, 102, 103, 104);
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], [1, 2, 3, 4]);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
+      }, 10);
+    });
+
+    it('5 arguments', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1(1, 2, 3, 4, 5);
+      assert.ok(!args.length);
+
+      setImmediate(function () {
+        assert.equal(args.length, 1);
+        callback1(101, 102, 103, 104, 105);
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], [1, 2, 3, 4, 5]);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
+      }, 10);
+    });
+
+    it('6 arguments', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1(1, 2, 3, 4, 5, 6);
+      assert.ok(!args.length);
+
+      setImmediate(function () {
+        assert.equal(args.length, 1);
+        callback1(101, 102, 103, 104, 105, 106);
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], [1, 2, 3, 4, 5, 6]);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
+      }, 10);
+    });
+
+    it('7 arguments', function (done) {
+      args = [];
+      var callback1 = callOnce(addArguments);
+      assert.ok(!args.length);
+      callback1(1, 2, 3, 4, 5, 6, 7);
+      assert.ok(!args.length);
+
+      setImmediate(function () {
+        assert.equal(args.length, 1);
+        callback1(101, 102, 103, 104, 105, 106, 107);
+        assert.equal(args.length, 1);
+        assert.deepEqual(args[0], [1, 2, 3, 4, 5, 6, 7]);
+
+        setImmediate(function () {
+          assert.equal(args.length, 1);
+          done();
+        }, 10);
+      }, 10);
+    });
   });
 });
