400c4b377d04b9790a46c60a2b39f1529493e787
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-19 09:11:28 -0700

    Moved to use fixed arguments

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ var nextTick = require('next-tick');
 
 module.exports = function callOnceNextTick(callback) {
   var called = false;
-  return function () {
+  return function (arg1, arg2, arg3, arg4, arg5, arg6) {
     if (called) return;
     called = true;
 
-    var self = this;
     var args = arguments;
     nextTick(function () {
-      callback.apply(self, args);
+      switch (arguments.length) {
+        case 3:
+          return callback(arg1);
+        case 4:
+          return callback(arg1, arg2);
+        case 5:
+          return callback(arg1, arg2, arg3);
+        case 6:
+          return callback(arg1, arg2, arg3, arg4);
+        case 7:
+          return callback(arg1, arg2, arg3, arg4, arg5);
+        case 8:
+          return callback(arg1, arg2, arg3, arg4, arg5, arg6);
+        default:
+          return callback.apply(null, args);
+      }
     });
   };
 };
