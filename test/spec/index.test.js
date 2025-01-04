139df4272f0fd1c543dfe177df36142f57056940
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-04 00:51:01 -0800

    Migrate to Biome and update build toolchain

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -1,170 +1,171 @@
-var assert = require('assert');
-var nextTick = require('next-tick');
+const assert = require('assert');
+const nextTick = require('next-tick');
 
-var once = require('../..');
+const once = require('call-once-next-tick');
 
-describe('once', function () {
-  describe('does not call multiple times', function () {
-    var args = [];
+describe('once', () => {
+  describe('does not call multiple times', () => {
+    let args = [];
 
     function addArguments() {
+      // biome-ignore lint/style/noArguments: <explanation>
       args.push(Array.prototype.slice.call(arguments, 0));
     }
 
-    it('0 arguments', function (done) {
+    it('0 arguments', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1();
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1();
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], []);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
       }, 10);
     });
 
-    it('1 argument', function (done) {
+    it('1 argument', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1(1);
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1(101);
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], [1]);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
       }, 10);
     });
 
-    it('2 arguments', function (done) {
+    it('2 arguments', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1(1, 2);
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1(101, 102);
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], [1, 2]);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
       }, 10);
     });
 
-    it('3 arguments', function (done) {
+    it('3 arguments', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1(1, 2, 3);
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1(101, 102, 103);
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], [1, 2, 3]);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
       }, 10);
     });
 
-    it('4 arguments', function (done) {
+    it('4 arguments', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1(1, 2, 3, 4);
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1(101, 102, 103, 104);
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], [1, 2, 3, 4]);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
       }, 10);
     });
 
-    it('5 arguments', function (done) {
+    it('5 arguments', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1(1, 2, 3, 4, 5);
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1(101, 102, 103, 104, 105);
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], [1, 2, 3, 4, 5]);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
       }, 10);
     });
 
-    it('6 arguments', function (done) {
+    it('6 arguments', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1(1, 2, 3, 4, 5, 6);
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1(101, 102, 103, 104, 105, 106);
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], [1, 2, 3, 4, 5, 6]);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
       }, 10);
     });
 
-    it('7 arguments', function (done) {
+    it('7 arguments', (done) => {
       args = [];
-      var callback1 = once(addArguments);
+      const callback1 = once(addArguments);
       assert.ok(!args.length);
       callback1(1, 2, 3, 4, 5, 6, 7);
       assert.ok(!args.length);
 
-      nextTick(function () {
+      nextTick(() => {
         assert.equal(args.length, 1);
         callback1(101, 102, 103, 104, 105, 106, 107);
         assert.equal(args.length, 1);
         assert.deepEqual(args[0], [1, 2, 3, 4, 5, 6, 7]);
 
-        nextTick(function () {
+        nextTick(() => {
           assert.equal(args.length, 1);
           done();
         }, 10);
