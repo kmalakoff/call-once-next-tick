022753e66da3f1c2f2b3be02cb16d7ade1ef6853
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:20 -0700

    Fix broken types

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ export default function once(fn: Function) {
           return fn(arg1, arg2, arg3, arg4, arg5, arg6);
         default:
           // biome-ignore lint/complexity/noArguments: Apply arguments
-          return fn.apply(null, arguments);
+          return fn.apply(null, arguments as unknown as unknown[]);
       }
     });
   };
